d952ba624abfd5481b5dd6d4ceeef13bd17cdc12
Jessica Peters <jessicapeters@Jessicas-MacBook-Air.local>
2018-10-01 21:13:10 -0500

    Header Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+// Includes
+import './Assets/CSS/default.min.css'
+
+// Components
+import Header from './Components/headerComponent/header'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <Header />
 
       </div>
     );
